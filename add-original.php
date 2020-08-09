246ab3d8f327124d0e93eff9dee7e53ef9a1f82f
Egi Mata <egi.mata1@gmail.com>
2020-08-09 06:24:02 -0700

    made changes in add-original

--- add-original.php
+++ add-original.php
@@ -1,48 +1,53 @@
 <?php
 
 // first you have to get both input files in separate variables
-$video = $_FILES["video"]["name"];
+
 $image = $_FILES["image"]["name"];
 
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["image"]["name"]);
-$target_file1 = $target_dir . basename($_FILES["video"]["name"]);
+
 $uploadOk = 1;
 $imageFileType1 = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 $imageFileType2 = strtolower(pathinfo($target_file1,PATHINFO_EXTENSION));
 
 if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
-    echo "<hr>The file ". basename( $_FILES["image"]["name"]). " has been uploaded.";
+    echo "<hr>The file ". basename( $_FILES["image"]["name"]). " has been uploaded.\n";
   }
 
-if (move_uploaded_file($_FILES["video"]["tmp_name"], $target_file1)) {
-    echo "<hr>The file ". basename( $_FILES["video"]["name"]). " has been uploaded.<hr>";
-  }
 
-// then you have to resize the selected image to lower resolution
-$command = "ffmpeg -i 'uploads/" . $image . "' -s 128x128 'uploads/output.jpeg'";
 
+// crop the image to ig size and scale it
+
+$command = 'ffmpeg -i "uploads/' . $image . '" -vf "scale=(iw*sar)*max(1080/(iw*sar)\,1080/ih):ih*max(1080/(iw*sar)\,1080/ih), crop=1080:1080" "uploads/output.png"';
 // execute that command
 system($command);
 
-echo "Overlay has been resized";
+echo "Overlay has been resized<p></p>";
 
-// both input files has been selected
-$command = "ffmpeg -i 'uploads/" . $video . "' -i 'uploads/output.jpeg'";
+// convert image to 15 sec video and zoom image
 
-// now apply the filter to select both files
-// it must enclose in double quotes
-// [0:v] means first input which is video
-// [1:v] means second input which is resized image
-$command .= " -filter_complex \"[0:v][1:v]";
-
-// now we need to tell the position of overlay in video
-$command .= " overlay=25:25\""; // closing double quotes
-
-// save in a separate output file
-$command .= " -c:a copy 'uploads/output.mp4'";
-
-// execute the command
+$command = 'ffmpeg -r 1/15 -i uploads/output.png -c:v libx264 -vf fps=25 -pix_fmt yuv420p uploads/out.mp4';
 system($command);
 
-echo "Overlay has been added";
+echo "Image transformed to video<p></p>";
+
+// add overlay image to top right (logo)
+$command = 'ffmpeg -i uploads/out.mp4 -i uploads/descript.png -filter_complex "[0:v][1:v] overlay=25:25:enable=\'between(t,0,15)\'" -pix_fmt yuv420p -c:a copy uploads/overlay.mp4';
+system($command);
+
+echo "Logo overlay has been added<p></p>";
+
+// add overlay movable image (border) from bottom to up (depending on title size)
+
+$command = 'ffmpeg -i uploads/overlay.mp4 -ignore_loop 0 -i uploads/border.gif -filter_complex "[1:v]scale=1920:-1 [ovrl], [0:v][ovrl] overlay=0:500:enable=\'between(t,0,15)\'" -c:a copy uploads/overlayborder.mp4';
+system($command);
+
+echo "Overlay has been added<p></p>";
+
+// add title with opacity and overlayed 
+
+$command = 'ffmpeg -i uploads/overlayborder.mp4 -filter_complex "[0:v]drawtext=fontfile=OpenSans-Regular.ttf:text=\'Ruku Ruku osh i mollRuku Ruku osh i mollRuku Ruku osh i mollRuku Ruku osh i mollRuku Ruku osh i mollRuku Ruku osh i mollRuku Ruku osh i mollRuku Ruku osh i mollRuku Ruku osh i mollRuku Ruku osh i moll\':fontsize=30:fontcolor=ffffff:alpha=\'if(lt(t,1),0,if(lt(t,3.5),(t-3)/0.5,if(lt(t,13.5),1,if(lt(t,14),(0.5-(t-13.5))/0.5,0))))\':x=30:y=700" uploads/overlayborderandtext.mp4';
+system($command);
+
+echo "<p></p>Text Overlay has been added\n\n";
--- add.php
+++ add.php
@@ -1,12 +1,24 @@
-<!DOCTYPE html>
-<html>
-<body>
+<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
-  Select image to upload:
-  <input type="file" name="fileToUpload" id="fileToUpload">
-  <input type="submit" value="Upload Image" name="submit">
-</form>
+<div class="container" style="margin-top: 100px;">
+	<div class="row">
+		<div class="offset-md-3">
+			
+			<!-- creating the form -->
+			<!-- apply encoding type to process file -->
+			<form method="POST" action="add-original.php" enctype="multipart/form-data">
 
-</body>
-</html>
+				<!-- creating image input file -->
+				<div class="form-group">
+					<label>Select image</label>
+					<input type="file" name="image" class="form-control">
+				</div>
+
+				<!-- create submit button -->
+				<input type="submit" class="btn btn-primary" value="Add overlay">
+
+			</form>
+
+		</div>
+	</div>
+</div>
