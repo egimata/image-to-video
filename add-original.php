ef6673fe2aa5c8e0d993d02f5ff7932badc29794
egimata <egi.mata1@gmail.com>
2020-08-10 08:57:26 -0700

    made changes

--- add-original.php
+++ add-original.php
@@ -1,25 +1,74 @@
 <?php
 
-// first you have to get both input files in separate variables
 
-$image = $_FILES["image"]["name"];
+function getTitleAndImg($url){
 
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["image"]["name"]);
+  error_reporting(0);
 
-$uploadOk = 1;
-$imageFileType1 = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-$imageFileType2 = strtolower(pathinfo($target_file1,PATHINFO_EXTENSION));
+  $html = file_get_contents($url);
+  $doc = new DOMDocument();
+  $doc->loadHTML($html);
 
-if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
-    echo "<hr>The file ". basename( $_FILES["image"]["name"]). " has been uploaded.\n";
+  $image = '';
+  $title = '';
+  // fetch og:tags
+  foreach( $doc->getElementsByTagName('meta') as $m ){
+
+        // if had property
+        if( $m->getAttribute('property') ){
+
+            $prop = $m->getAttribute('property');
+
+            // here search only og:tags
+            if( preg_match("/og:/i", $prop) ){
+
+              //get first image according to og content
+                  if ($m->getAttribute('property') == 'og:image'){
+                      $image = $m->getAttribute('content');
+                  }
+
+
+                  if ($m->getAttribute('property') == 'og:title'){
+                      $title = $m->getAttribute('content');
+                  }
+
+        }
+      }
+        // end if had property
+
   }
+  // end foreach
+  echo $title.'<hr>';
+  echo $image.'<hr>';
+  echo '<img src="'.$image.'">';
+  echo '<hr>';
 
+}
+
+
+$url = ("https://www.balkanweb.com/manastirliu-me-ekipin-e-mjekeve-italiane-dhe-shqiptare-fuqizojme-kapacitetet-njerezore-400-paciente-u-shpetuan-ne-terapine-intensive-ne-infektiv/");
+
+echo getTitleAndImg($url);
+
+
+// first you have to get both input files in separate variables
+// $image = $_FILES["image"]["name"];
+
+// $target_dir = "uploads/";
+// $target_file = $target_dir . basename($_FILES["image"]["name"]);
+
+// $uploadOk = 1;
+// $imageFileType1 = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// $imageFileType2 = strtolower(pathinfo($target_file1,PATHINFO_EXTENSION));
+
+// if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+//     echo "<hr>The file ". basename( $_FILES["image"]["name"]). " has been uploaded.";
+//   }
 
 
 // crop the image to ig size and scale it
 
-$command = 'ffmpeg -i "uploads/' . $image . '" -vf "scale=(iw*sar)*max(1080/(iw*sar)\,1080/ih):ih*max(1080/(iw*sar)\,1080/ih), crop=1080:1080" "uploads/output.png"';
+$command = 'ffmpeg -i '.$image.' -vf "scale=(iw*sar)*max(1080/(iw*sar)\,1080/ih):ih*max(1080/(iw*sar)\,1080/ih), crop=1080:1080" "uploads/output.png"';
 // execute that command
 system($command);
 
@@ -48,14 +97,14 @@ echo "Logo overlay has been added<p></p>";
 
 // add overlay movable image (border) from bottom to up (depending on title size)
 
-$command = 'ffmpeg -i uploads/overlay.mp4 -ignore_loop 0 -i uploads/border.gif -filter_complex "[1:v]scale=1920:-1 [ovrl], [0:v][ovrl] overlay=0:500:enable=\'between(t,0,15)\'" -c:a copy uploads/overlayborder.mp4';
+$command = 'ffmpeg -i uploads/overlay.mp4 -ignore_loop 0 -i uploads/border.gif -filter_complex "[1:v]scale=1920:-1 [ovrl], [0:v][ovrl] overlay=0:650:enable=\'between(t,0,15)\'" -c:a copy uploads/overlayborder.mp4';
 system($command);
 
 echo "Overlay has been added<p></p>";
 
 // add title with opacity and overlayed 
 
-$command = 'ffmpeg -i uploads/overlayborder.mp4 -filter_complex "[0:v]drawtext=fontfile=OpenSans-Regular.ttf:text=\'Ruku Ruku osh i mollRuku Ruku osh i mollRuku Ruku osh i <p></p> mollRuku Ruku osh i mollRuku Ruku osh i mollRuku Ruku osh i mollRuku Ruku osh i mollRuku Ruku osh i mollRuku Ruku osh i <p></p>mollRuku Ruku osh i moll\':fontsize=40:fontcolor=ffffff:alpha=\'if(lt(t,1),0,if(lt(t,3.5),(t-3)/0.5,if(lt(t,13.5),1,if(lt(t,14),(0.5-(t-13.5))/0.5,0))))\':x=30:y=600" uploads/overlayborderandtext.mp4';
+$command = 'ffmpeg -i uploads/overlayborder.mp4 -filter_complex "[0:v]drawtext=fontfile=OpenSans-Regular.ttf:text=\'Teksti Ktu\':fontsize=40:fontcolor=ffffff:alpha=\'if(lt(t,1),0,if(lt(t,3.5),(t-3)/0.5,if(lt(t,13.5),1,if(lt(t,14),(0.5-(t-13.5))/0.5,0))))\':x=30:y=600" uploads/overlayborderandtext.mp4';
 system($command);
 
 echo "<p></p>Text Overlay has been added\n\n";
